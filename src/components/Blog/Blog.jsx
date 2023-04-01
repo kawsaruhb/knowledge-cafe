4308951898958bb850556f177c0bb4b68bec5c4f
kawsaruhb <kawsaruhb@gmail.com>
2023-04-01 21:01:33 +0600

    mobile responsiveness added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -37,11 +37,14 @@ const Blog = () => {
                 <h4>1. Props vs state?</h4>
                 <p>Ans: State is referred to the local state of the component which cannot be accessed and modified outside of the component and only can be used & modified inside the component. Props, on the other hand,make components reusable by giving components the ability to receive data from the parent component in the form of props.</p>
 
-                <h4>1. How does useState work??</h4>
+                <h4>2. How does useState work??</h4>
                 <p>Ans: useState is React Hook that allows you to add state to a functional component. It returns an array with two values: the current state and a function to update it. The Hook takes an initial state value as an argument and returns an updated state value whenever the setter function is called.</p>
 
-                <h4>1. Props vs state?</h4>
-                <p>Ans: State is referred to the local state of the component which cannot be accessed and modified outside of the component and only can be used & modified inside the component. Props, on the other hand,make components reusable by giving components the ability to receive data from the parent component in the form of props.</p>
+                <h4>3. Purpose of useEffect other than fetching data?</h4>
+                <p>Ans: The useEffect in react js allows you to perform side effects in your components. The react useEffect examples of side effects include retrieving data, direct DOM updates, and timers. The second argument is optional.</p>
+
+                <h4>4. How Does React work?</h4>
+                <p>Ans: React is a JavaScript library that creates user interfaces in a predictable and efficient way using declarative code. You can use it to help build single page applications and mobile apps, or to build complex apps if you utilize it with other libraries. <br /> ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. </p>
             </div>
         </div>
     );
